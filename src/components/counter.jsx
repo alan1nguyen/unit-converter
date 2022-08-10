d4804aeb75cada6a0559132caa39516af3a5dce3
alan1nguyen <nguyenalan101@gmail.com>
2022-08-09 19:47:31 -0500

    Add Bootstrap

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Unit Converter</h1>
+                <button>Convert</button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Counter;
