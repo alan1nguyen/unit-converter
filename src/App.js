2f8acc2af0aee6a27bd4bae6fe07b0da9eadafbb
alan1nguyen <nguyenalan101@gmail.com>
2022-08-12 20:39:17 -0500

    Add Inches and Millimeters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [inches, setInches] = useState(0);
+  const [mmToInch, setmmToInch] = useState(0);
+  const [millimeters, setMillimeters] = useState(0);
+  const [inchToMillimeter, setinchToMillimeter ] = useState(0);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const formValid = +inches >= 0;
+    if (!formValid) {
+      alert("Input is invalid!");
+    }
+    setinchToMillimeter(inches*25.4);
+    setmmToInch(millimeters/25.4);
+    //setCentimeters((+feet + +inches / 12) * 12 * 2.54);
+  };
+
   return (
-    //<h1>Hello World</h1>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
+    <div>
+      <h1>Unit Converter</h1>
+      <form onSubmit={submit}>
+        <div>
+          <label>Inches</label>
+          <input value={inches} onChange={(e) => setInches(e.target.value)} />
+          <button type="submit">Convert to mm</button>
+        </div>
+        <p>{parseFloat(inchToMillimeter.toFixed(4))} mm</p>
+
+        <div>
+          <label>Millimeters</label>
+          <input value={millimeters} onChange={(e) => setMillimeters(e.target.value)} />
+          <button type="submit">Convert to inches</button>
+        </div>
+        <p>{parseFloat(mmToInch.toFixed(4))} inches</p>
+
+      </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
-  </React.StrictMode> //App
+    <App/>
+  </React.StrictMode> 
 );
